90fb7c588c1c71d9a6ff8a101206c47743127da3
Ozan Yurekli <ozannyurekli99@gmail.com>
2024-07-07 13:20:37 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
 
@@ -126,6 +127,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.remove();
+
+    renderPaymentSummary();
   });
 
 });
@@ -138,6 +141,7 @@ document.querySelectorAll('.js-delivery-option')
     const {productId,deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId,deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
   });
 });
 }
